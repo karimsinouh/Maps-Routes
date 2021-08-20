2db3b0e3348e9b130c39add31f1f20c3760b850a
karim <sinouhkar@gmail.com>
2021-08-20 14:10:57 -0700

    attach map view to activity's lifecycle

--- app/src/main/java/com/karimsinouh/mapsroutes/MainActivity.kt
+++ app/src/main/java/com/karimsinouh/mapsroutes/MainActivity.kt
@@ -2,16 +2,57 @@ package com.karimsinouh.mapsroutes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.maps.GoogleMap
 import com.karimsinouh.mapsroutes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityMainBinding
 
+    private var map:GoogleMap ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.mapView.onCreate(savedInstanceState)
+
+        binding.mapView.getMapAsync {
+            map=it
+            Log.d("wtf",it.toString())
+        }
+
+
     }
+
+
+    //lifecycle stuff
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
 
 }
